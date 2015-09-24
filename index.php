15831fa23ebf6b24e4a723147d51a420e041a688
Casca <jscasca@gmail.com>
2015-09-24 00:06:49 -0400

    Changed favicon

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if(!isset($_SESSION["user"])){
 	<meta description="Description" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	
-	<link rel="shortcut icon" href="img/favicon.png"> 
+	<link rel="shortcut icon" href="img/favicon.ico"> 
 	
 	<!-- bootstrap -->
 	<link href="css/bootstrap.min.css" rel="stylesheet" >
